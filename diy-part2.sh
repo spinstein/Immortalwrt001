2f1b260a364494aacadd390271fcbea933fea80b
spinstein <2717896+spinstein@users.noreply.github.com>
2025-06-23 18:17:26 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,14 +10,10 @@
 # See /LICENSE for more information.
 #
 
-# 修改默认IP
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 
-# 修改时区为上海
 sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
 
-# 优化DNS设置
 sed -i '/option resolvfile/a\        list server '\''127.0.0.1#5053'\''' package/network/services/dnsmasq/files/dhcp.conf
 
-# 添加docker用户组
 sed -i '/system.ntp/a\\t\tgroup add docker' package/base-files/files/etc/init.d/boot
