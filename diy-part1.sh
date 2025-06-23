03b870148ca3bcb18424ed8fc508f5005cde6d75
spinstein <2717896+spinstein@users.noreply.github.com>
2025-06-23 19:12:27 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,3 +13,6 @@
 echo 'src-git openclash https://github.com/vernesong/OpenClash' >> feeds.conf.default
 echo 'src-git adguardhome https://github.com/rufengsuixing/luci-app-adguardhome' >> feeds.conf.default
 echo 'src-git dockerman https://github.com/lisaac/luci-app-dockerman' >> feeds.conf.default
+
+# Add kernel patches for J4125 stability
+echo 'src-git kernel_patches https://github.com/coolsnowwolf/lede;master' >> feeds.conf.default
