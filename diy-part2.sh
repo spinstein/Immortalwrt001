943f28620d65876b8f4ea78282cdbdcc16b2c540
spinstein <2717896+spinstein@users.noreply.github.com>
2025-06-23 19:03:41 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,11 +15,11 @@ sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generat
 # Set timezone to CST-8 (Shanghai)
 sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
 
-# Set AdGuardHome as upstream DNS
-sed -i '/option resolvfile/a\        list server '\''127.0.0.1:5353'\''' package/network/services/dnsmasq/files/dhcp.conf
+# Fix AdGuardHome upstream DNS syntax
+sed -i '/option resolvfile/a\\t\tlist server 127.0.0.1#5353' package/network/services/dnsmasq/files/dhcp.conf
 
 # Create docker group with correct syntax
-sed -i '/system.ntp/a\\t\tgroupadd -g 1000 docker || true' package/base-files/files/etc/init.d/boot
+sed -i '/system.ntp/a\\t\tgroup_add 1000 docker' package/base-files/files/etc/init.d/boot
 
 # Change AdGuardHome listening port
 find package/ -type f -name 'AdGuardHome' -exec sed -i 's/0.0.0.0:53/0.0.0.0:5353/g' {} +
